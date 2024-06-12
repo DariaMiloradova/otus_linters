418eb3cdd69d902978440f0f79cc360d28c36e84
Daria Miloradova <darya.miloradova@mail.ru>
2024-06-12 13:13:32 +0300

    add new package - src with homework

---
+++ src/figure.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+
+
+class Figure(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def get_area(self):
+        pass
+
+    def add_area(self, other_figure):
+        if not isinstance(other_figure, Figure):
+            raise ValueError("Нужно передать фигуру")
+        return self.get_area() + other_figure.get_area()
+
